5de65e822144de73fe44841741e6d9753bda8e33
Khinshan Khan <khanshan501@gmail.com>
2020-01-07 15:56:58 -0500

    finished decrement

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class ContactCard extends React.Component {
 
   renderFromProps() {
     return Object.keys(this.props).map(key => (
-      <div>
+      <div key={key}>
         {key[0].toUpperCase() + key.slice(1)}: {this.props[key]}
         <br />
       </div>
@@ -24,7 +24,7 @@ class ContactCard extends React.Component {
 
   render() {
     return (
-      <div className="App">
+      <div>
         <h3>Contact Card:</h3>
         <div>{this.renderFromProps()} </div>
       </div>
@@ -33,42 +33,107 @@ class ContactCard extends React.Component {
 }
 
 ContactCard.propTypes = {
-  name: PropTypes.string,
-  personal: PropTypes.string,
-  work: PropTypes.string.isRequired,
-  email: PropTypes.string
+  name: PropTypes.string.isRequired,
+  personal: PropTypes.string.isRequired,
+  work: PropTypes.string,
+  email: PropTypes.string.isRequired
 };
 
-ContactCard.defaultProps = {
-  name: "name",
-  personal: "2",
-  work: "3",
-  email: "4"
+class Decrement extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    };
+
+    this._myHandler = this._myHandler.bind(this);
+    this.decrease = this.decrease.bind(this);
+    this.increase = this.increase.bind(this);
+  }
+
+  _myHandler(props) {
+    console.log(props);
+  }
+
+  myButton(context, color, callback) {
+    return (
+      <button
+        type="button"
+        style={{
+          color: "white",
+          backgroundColor: color,
+          padding: "15px 32px",
+          borderRadius: "50%"
+        }}
+        onClick={callback}
+      >
+        {context}
+      </button>
+    );
+  }
+
+  decrease() {
+    if (this.state.counter === 0) {
+      alert("Cannot be less than zero");
+    } else {
+      this.setState({ counter: this.state.counter - 1 });
+    }
+  }
+
+  increase() {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h3>Decrement</h3>
+        {this.state.counter}
+        <br />
+        {this.myButton("(-)", "red", this.decrease)}
+        {this.myButton("(+)", "blue", this.increase)}
+      </div>
+    );
+  }
+}
+
+Decrement.propTypes = {
+  counter: PropTypes.number.isRequired
 };
 
 export default class App extends React.Component {
   render() {
     return (
-      <header className="App-header">
-        <ContactCard
-          name="First Guy"
-          personal="(123) 456 7890"
-          work="(123) 456 7891"
-          email="first.guy@gmail.com"
-        />
-        <ContactCard
-          name="Second Guy"
-          personal="(231) 456 7890"
-          work="(231) 456 7891"
-          email="second.guy@gmail.com"
-        />
-        <ContactCard
-          name="Third Guy"
-          personal="(321) 456 7890"
-          work="(321) 456 7891"
-          email="third.guy@gmail.com"
-        />
-      </header>
+      <div id="app">
+        <header className="App-header">
+          <h1>Contact Card & Decrement</h1>
+          <h3>Scroll Down!</h3>
+        </header>
+        <header className="App-header">
+          <ContactCard
+            name="John Racs"
+            personal="(123) 456 7890"
+            work="(123) 456 7891"
+            email="john.racs@gmail.com"
+          />
+          <ContactCard
+            name="Mark Scar"
+            personal="(213) 456 7890"
+            work="(213) 456 7891"
+            email="mark.scar@gmail.com"
+          />
+          <ContactCard
+            name="Cathy Cars"
+            personal="(321) 456 7890"
+            work="(321) 456 7891"
+            email="cathy.cars@gmail.com"
+          />
+        </header>
+        <header className="App-header">
+          <Decrement counter={0} />
+        </header>
+      </div>
     );
   }
 }
