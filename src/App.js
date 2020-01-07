415ee3b84e268203fbdedf5c795961f96d91826e
Khinshan Khan <khanshan501@gmail.com>
2020-01-07 14:04:36 -0500

    finished contact card

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ContactCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this._myHandler = this._myHandler.bind(this);
+  }
+
+  _myHandler(props) {
+    console.log(props);
+  }
+
+  renderFromProps() {
+    return Object.keys(this.props).map(key => (
+      <div>
+        {key[0].toUpperCase() + key.slice(1)}: {this.props[key]}
+        <br />
+      </div>
+    ));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h3>Contact Card:</h3>
+        <div>{this.renderFromProps()} </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+ContactCard.propTypes = {
+  name: PropTypes.string,
+  personal: PropTypes.string,
+  work: PropTypes.string.isRequired,
+  email: PropTypes.string
+};
+
+ContactCard.defaultProps = {
+  name: "name",
+  personal: "2",
+  work: "3",
+  email: "4"
+};
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <header className="App-header">
+        <ContactCard
+          name="First Guy"
+          personal="(123) 456 7890"
+          work="(123) 456 7891"
+          email="first.guy@gmail.com"
+        />
+        <ContactCard
+          name="Second Guy"
+          personal="(231) 456 7890"
+          work="(231) 456 7891"
+          email="second.guy@gmail.com"
+        />
+        <ContactCard
+          name="Third Guy"
+          personal="(321) 456 7890"
+          work="(321) 456 7891"
+          email="third.guy@gmail.com"
+        />
+      </header>
+    );
+  }
+}
