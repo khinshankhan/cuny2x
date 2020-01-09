d092d91624832c2b56981469db6ee206cde93973
Samson <samson50wu@gmail.com>
2020-01-09 03:13:32 -0500

    Installed axios, able to receive a response

--- src/Components/Result.js
+++ src/Components/Result.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './Result.css';
 
 function Result(props) {
-  return <div>Here is a result component</div>
+  return (
+    <div>Here is a result component
+      
+    </div>
+  );
 }
 
 export default Result;
